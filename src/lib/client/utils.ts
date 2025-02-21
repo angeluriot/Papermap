d878586de71a1ef9e1602ff75c1c5e6ee79b0a86
angeluriot <angel.uriot@gmail.com>
2025-02-21 23:17:49 +0100

    Tsconfig

--- src/lib/client/utils.ts
+++ src/lib/client/utils.ts
@@ -1,5 +1,5 @@
 export const constants = {
 	DEV: import.meta.env.DEV,
 	PROD: import.meta.env.PROD,
-	BASE_URL: import.meta.env.DEV ? 'http://localhost:5173' : (import.meta.env.VITE_DOMAIN as string ?? 'https://example.com'),
+	BASE_URL: import.meta.env.DEV ? 'http://localhost:5173' : (import.meta.env['VITE_DOMAIN'] as string ?? 'https://example.com'),
 };
