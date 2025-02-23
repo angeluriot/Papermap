a5a412864170a20199375acadd16fccd7915e619
angeluriot <angel.uriot@gmail.com>
2025-02-23 02:36:30 +0100

    Symplify GitHub API

--- src/lib/server/types.ts
+++ src/lib/server/types.ts
@@ -1,4 +1,14 @@
-enum JournalType
+export enum PRLabel
+{
+	JournalChange = 'journal change',
+	JournalDeletion = 'journal deletion',
+	NewJournal = 'new journal',
+	MapChange = 'map change',
+	MapDeletion = 'map deletion',
+	NewMap = 'new map',
+}
+
+export enum JournalType
 {
 	Journal = 'Journal',
 	BookSeries = 'Book Series',
--- src/routes/maps/[map]/edit/+server.ts
+++ src/routes/maps/[map]/edit/+server.ts
@@ -1,55 +1,33 @@
 import { json } from '@sveltejs/kit';
-import { constants as C, get_random_string } from '$lib/server/utils';
+import { constants as C } from '$lib/server/utils';
 import { promises as fs } from 'fs';
-import { join } from 'path'
-import type { Octokit } from '@octokit/rest';
-import { init_client, create_branch, delete_branch, update_file, create_pull_request } from '$lib/server/github/api';
+import { join } from 'path';
+import { create_pull_request } from '$lib/server/github/actions';
+import { PRLabel } from '$lib/server/types';
 
 
 export async function POST({ params, request }: { params: { map: string }, request: Request }): Promise<Response>
 {
-	const task_id = get_random_string();
 	const data = await request.json();
 
 	console.log('Data:', data);
 	console.log('Used path:', params.map);
 
-	let pr_url: string;
-
-	if (C.DEV)
+	if (false && C.DEV)
 	{
 		await fs.writeFile(join(C.LIB_DIR, `server/jsons/maps/${params.map}/question.json`), JSON.stringify(data, null, '\t') + '\n');
-		pr_url = '';
+		return json({ pr_url: '' });
 	}
 
-	else
-	{
-		const branch = `edit/${params.map.replace('_', '-')}/id-${task_id}`;
-		let client: Octokit;
-
-		try
-		{
-			client = init_client();
-			await create_branch(client, branch);
-		}
-
-		catch (error: any)
-		{
-			throw error;
-		}
-
-		try
-		{
-			await update_file(client, branch, `src/lib/server/jsons/maps/${params.map}/question.json`, JSON.stringify(data, null, '\t') + '\n', 'Update question.json');
-			pr_url = await create_pull_request(client, branch, 'Update question.json', 'Test description', 'map change');
-		}
-
-		catch (error: any)
-		{
-			await delete_branch(client, branch);
-			throw error;
-		}
-	}
+	const pr_url = await create_pull_request({
+		branch_name: `edit/${params.map.replace('_', '-')}`,
+		file_path: `src/lib/server/jsons/maps/${params.map}/question.json`,
+		new_content: JSON.stringify(data, null, '\t') + '\n',
+		commit_message: 'Update question.json',
+		title: 'Update question.json',
+		description: 'Test description',
+		label: PRLabel.MapChange,
+	});
 
 	return json({ pr_url });
 }
