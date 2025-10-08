9e0bb85f7f46b733768d1d1156b1af3936371cf1
angeluriot <angel.uriot@gmail.com>
2025-10-08 20:14:51 +0200

    Better seeds

--- src/lib/display/graph/utils.ts
+++ src/lib/display/graph/utils.ts
@@ -11,6 +11,9 @@ const Y_MIN_PADDING = 8;
 const Y_MAX_PADDING_LARGE = 20;
 const Y_MAX_PADDING_TIGHT = 25;
 const OVERFLOW_PADDING = 20;
+const INIT_MIN_YEAR = 1950;
+const INIT_MIN_SCORE = -0.02;
+const INIT_MAX_SCORE = 1.02;
 
 
 export function get_stats(map: Map, width: number, height: number, top_margin_scale: number = 1, bottom_margin_scale: number = 1): GraphStats
@@ -18,7 +21,7 @@ export function get_stats(map: Map, width: number, height: number, top_margin_sc
 	const scale = (width + height) * 0.001;
 
 	const years = Object.values(map.papers).map(paper => paper.year + (paper.override_seed !== undefined ? paper.override_seed : seedrandom(paper.title).quick()));
-	let min_year = 1950;
+	let min_year = INIT_MIN_YEAR;
 	let max_year = new Date().getFullYear();
 
 	if (years.length > 0)
@@ -36,8 +39,8 @@ export function get_stats(map: Map, width: number, height: number, top_margin_sc
 	const pad_year = (max_year - min_year) / (width ** 0.7);
 
 	const scores = Object.values(map.papers).map(paper => paper.score);
-	let min_score = 0;
-	let max_score = 1;
+	let min_score = INIT_MIN_SCORE;
+	let max_score = INIT_MAX_SCORE;
 
 	if (scores.length > 0)
 	{
@@ -54,14 +57,14 @@ export function get_stats(map: Map, width: number, height: number, top_margin_sc
 	const pad_score = (max_score - min_score) / (height ** 0.7);
 	const final_min_year = min_year - pad_year * X_MIN_PADDING;
 	const final_max_year = max_year + pad_year * X_MAX_PADDING;
-	const final_min_score = Math.min(min_score - pad_score * Y_MIN_PADDING * bottom_margin_scale, 0);
-	const final_max_score = Math.max(max_score + pad_score * (width <= 600 ? Y_MAX_PADDING_TIGHT : Y_MAX_PADDING_LARGE) * top_margin_scale, 1);
+	const final_min_score = Math.min(min_score - pad_score * Y_MIN_PADDING * bottom_margin_scale, INIT_MIN_SCORE);
+	const final_max_score = Math.max(max_score + pad_score * (width <= 600 ? Y_MAX_PADDING_TIGHT : Y_MAX_PADDING_LARGE) * top_margin_scale, INIT_MAX_SCORE);
 
 	const stats = {
-		min_year: years.length > 0 ? final_min_year : 1950,
+		min_year: years.length > 0 ? final_min_year : INIT_MIN_YEAR,
 		max_year: years.length > 0 ? final_max_year : new Date().getFullYear(),
-		min_score: scores.length > 0 ? final_min_score : 0,
-		max_score: scores.length > 0 ? final_max_score : 1,
+		min_score: scores.length > 0 ? final_min_score : INIT_MIN_SCORE,
+		max_score: scores.length > 0 ? final_max_score : INIT_MAX_SCORE,
 		width,
 		height,
 		scale,
